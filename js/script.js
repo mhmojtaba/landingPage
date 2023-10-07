428951b9658efaaa1d70837b03260b7192d8ba78
Mojtaba Mohammadi <mhmojtaba@gmail.com>
2023-10-07 23:36:56 +0330

    mobile menu js function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 //
+// const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+const mobileMenu = document.querySelector(".show");
+// document
+//   .querySelector(".mobile-menu-icon")
+//   .addEventListener("click", function () {
+//     alert("click");
+//   });
+
+// mobileMenuIcon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  //   console.log("click");
+  mobileMenu.classList.toggle("mobile-menu");
+}
